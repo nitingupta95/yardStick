6958da2835e0f5891812063c6970ca5f40e28419
nitingupta95 <ng61315@gmail.com>
2025-08-07 19:10:39 +0530

    after solving lint error

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,10 @@
-const AuthLayout = ({ children }) => {
+import type { ReactNode } from "react";
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+const AuthLayout = ({ children }: RootLayoutProps) => {
   return <div className="flex justify-center pt-40">{children}</div>;
 };
 
